79dc39d6bf31f612223a041acf2d3e0e3f5e3624
SimoSamaa <rayanidbrahim@gmail.com>
2024-11-26 00:20:02 +0100

    improve debounce function

--- src/lib/debounce.ts
+++ src/lib/debounce.ts
@@ -1,35 +1,19 @@
 const debounce = <T extends unknown[]>(
   fn: (...args: T) => void,
-  delay: number,
-  immediate: boolean = false
+  delay: number
 ) => {
   let timeout: ReturnType<typeof setTimeout> | null = null;
 
-  return (...args: T) => {
-    const callNow = immediate && !timeout;
+  const debounced = (...args: T) => {
     if (timeout) clearTimeout(timeout);
-
-    timeout = setTimeout(() => {
-      timeout = null;
-      if (!immediate) fn(...args);
-    }, delay);
-
-    if (callNow) fn(...args);
+    timeout = setTimeout(() => fn(...args), delay);
   };
+
+  debounced.cancel = () => {
+    if (timeout) clearTimeout(timeout);
+  };
+
+  return debounced;
 };
-
-// const debounce = <T extends unknown[]>(
-//   fn: (...args: T) => void,
-//   delay: number
-// ) => {
-//   let timeout: ReturnType<typeof setTimeout>;
-
-//   return (...args: T) => {
-//     clearTimeout(timeout);
-//     timeout = setTimeout(() => {
-//       fn(...args);
-//     }, delay);
-//   };
-// };
 
 export default debounce;
